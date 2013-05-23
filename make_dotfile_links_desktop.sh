2b4a22b7fb65a6aaa06833024bc63a46c91421a2
Zach Brewer <zach.brewer@insightbb.com>
2013-05-23 01:03:00 -0400

    Added desktop files and changed to work with both desktops and laptops

---
+++ make_dotfile_links_desktop.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+#This script will make links for all of the dotfiles that are in the dotfiles git repo
+#For use on laptops
+
+ln -s bashrc $HOME/.bashrc
+ln -s conkyrc_desktop $HOME/.conkyrc
+ln -s ctags $HOME/.ctags
+ln -s i3_desktop $HOME/.i3
+ln -s vim $HOME/.vim
+ln -s vimrc $HOME/.vimrc
+ln -s xinitrc $HOME/.xinitrc
+ln -s Xresources $HOME/.Xresources
+ln -s zshrc $HOME/.zshrc
--- make_dotfile_links_laptop.sh
+++ make_dotfile_links_laptop.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 #This script will make links for all of the dotfiles that are in the dotfiles git repo
+#For use on laptops
 
 ln -s bashrc $HOME/.bashrc
-ln -s conkyrc $HOME/.conkyrc
+ln -s conkyrc_laptop $HOME/.conkyrc
 ln -s ctags $HOME/.ctags
-ln -s i3 $HOME/.i3
+ln -s i3_laptop $HOME/.i3
 ln -s vim $HOME/.vim
 ln -s vimrc $HOME/.vimrc
 ln -s xinitrc $HOME/.xinitrc
