b99d86741c5d29ed6d23c2fde8e7aa2c93048e3b
Zach Brewer <zach.brewer@insightbb.com>
2013-05-23 00:06:51 -0400

    Added a simple shell script to make links to all of the dotfiles in the git repo

---
+++ make_dotfile_links.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+#This script will make links for all of the dotfiles that are in the dotfiles git repo
+
+ln -s bashrc $HOME/.bashrc
+ln -s conkyrc $HOME/.conkyrc
+ln -s ctags $HOME/.ctags
+ln -s i3 $HOME/.i3
+ln -s vim $HOME/.vim
+ln -s vimrc $HOME/.vimrc
+ln -s xinitrc $HOME/.xinitrc
+ln -s Xresources $HOME/.Xresources
+ln -s zshrc $HOME/.zshrc
