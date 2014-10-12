7bfcbf042773c3b0dcebdd8b89f66e8952ce41ef
Zach Brewer <zbrewer2@illinois.edu>
2014-10-11 22:45:44 -0500

    Laptop specific changes

--- battery.py
+++ battery.py
@@ -7,8 +7,11 @@ encoding = locale.getdefaultlocale()[1]
 
 c = subprocess.Popen(["acpi", "-b"], stdout=subprocess.PIPE)
 output, err = c.communicate()
-d = output.decode(encoding).split(', ')[1]
-charge = float(d.strip()[:-1])
+d1 = output.decode(encoding).split('\n')[0].split(', ')[1]
+d2 = output.decode(encoding).split('\n')[1].split(', ')[1]
+charge1 = float(d1.strip()[:-1])
+charge2 = float(d2.strip()[:-1])
+charge = (charge1 + charge2)/2
 
 charge_threshold = int(math.ceil(charge / 10))
 
@@ -21,17 +24,22 @@ empty = (total_slots - len(filled)) * u'▹'
 out = (filled + empty).encode('utf-8')
 import sys
 
-color_green = '%{\033[32m%}'
-color_yellow = '%{\033[33m%}'
-color_red = '%{\033[31m%}'
-color_reset = '%{\033[0m%}'
+#color_green = '%{\033[32m%}'
+#color_yellow = '%{\033[33m%}'
+#color_red = '%{\033[31m%}'
+#color_reset = '%{\033[0m%}'
+
+color_green = '\033[32m'
+color_yellow = '\033[33m'
+color_red = '\033[31m'
+color_reset = '\033[0m'
+
 
 color_out = (
     color_green if len(filled) > 6
     else color_yellow if len(filled) > 4
     else color_red
 )
-
 out = color_out + out + color_reset
-# print(out)
-sys.stdout.write(out)
+print(out)
+#sys.stdout.write(out)
