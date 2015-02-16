0ede3225816334e95e572ffd3e2f145de848e532
Zach Brewer <zbrewer2@illinois.edu>
2015-02-16 01:30:23 -0600

    Updated dotfiles to work with my new monitor setup

--- i3_desktop/Scripts/MultipleMonitors.sh
+++ i3_desktop/Scripts/MultipleMonitors.sh
@@ -1,2 +1,3 @@
 xrandr --output DVI-0 --auto
-xrandr --output DVI-1 --auto --right-of DVI-0
+xrandr --output DisplayPort-1 --auto --right-of DVI-0
+xrandr --output DVI-1 --auto --right-of DisplayPort-1
